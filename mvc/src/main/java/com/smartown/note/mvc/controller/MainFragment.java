d493a7eb27e2f7f7aff7ad4c0362138f86e39a53
Tiger <1076192306@qq.com>
2016-08-04 21:01:33 +0800

    demo内容显示

--- mvc/src/main/java/com/smartown/note/mvc/controller/MainFragment.java
+++ mvc/src/main/java/com/smartown/note/mvc/controller/MainFragment.java
@@ -4,29 +4,57 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 
+import com.google.gson.Gson;
 import com.smartown.library.base.BaseFragment;
+import com.smartown.library.common.adapter.CommonAdapter;
+import com.smartown.library.common.adapter.OnItemClickListener;
+import com.smartown.library.common.adapter.ValueGetter;
 import com.smartown.note.mvc.R;
+import com.smartown.note.mvc.entity.EntityWeChatNews;
 import com.smartown.note.mvc.operator.WeChatOperator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import rx.Subscriber;
 
 /**
  * 作者：Tiger
- * <p/>
+ * <p>
  * 时间：2016-08-04 16:09
- * <p/>
+ * <p>
  * 描述：
  */
-public class MainFragment extends BaseFragment {
+public class MainFragment extends BaseFragment implements OnItemClickListener {
 
     private RecyclerView recyclerView;
+    private List<EntityWeChatNews> newses;
     private WeChatOperator operator;
+    private Gson gson;
+    private CommonAdapter<EntityWeChatNews> adapter;
+    private ValueGetter<EntityWeChatNews> valueGetter;
 
     @Override
     protected void init() {
+        newses = new ArrayList<>();
         operator = new WeChatOperator();
+        gson = new Gson();
+        adapter = new CommonAdapter<EntityWeChatNews>(getActivity(), newses);
+        valueGetter = new ValueGetter<EntityWeChatNews>() {
+            @Override
+            public String getValue(EntityWeChatNews news) {
+                return news.getTitle();
+            }
+        };
+        adapter.setValueGetter(valueGetter);
+        adapter.setOnItemClickListener(this);
         findViews(R.layout.common_recycler_list);
     }
 
@@ -38,6 +66,7 @@ public class MainFragment extends BaseFragment {
     @Override
     protected void initViews() {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -61,8 +90,29 @@ public class MainFragment extends BaseFragment {
 
             @Override
             public void onNext(String result) {
-                System.out.println(result);
+                if (!TextUtils.isEmpty(result)) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(result);
+                        if (jsonObject.optInt("code") == 200) {
+                            JSONArray jsonArray = jsonObject.optJSONArray("newslist");
+                            if (jsonArray != null) {
+                                int length = jsonArray.length();
+                                for (int i = 0; i < length; i++) {
+                                    newses.add(gson.fromJson(jsonArray.optString(i), EntityWeChatNews.class));
+                                }
+                                adapter.notifyDataSetChanged();
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
+    }
+
+    @Override
+    public void onItemClick(int position) {
+
     }
 }
---
+++ mvc/src/main/java/com/smartown/note/mvc/entity/EntityWeChatNews.java
@@ -0,0 +1,58 @@
+package com.smartown.note.mvc.entity;
+
+/**
+ * 作者：Tiger
+ * <p/>
+ * 时间：2016-08-04 20:34
+ * <p/>
+ * 描述：
+ */
+public class EntityWeChatNews {
+
+    private String ctime;
+    private String title;
+    private String description;
+    private String picUrl;
+    private String url;
+
+    public String getCtime() {
+        return ctime;
+    }
+
+    public void setCtime(String ctime) {
+        this.ctime = ctime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
