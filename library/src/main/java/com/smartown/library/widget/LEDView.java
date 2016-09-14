efa5174f6e5fd0917b68c26e69c70625d60670ad
Tiger <1076192306@qq.com>
2016-09-14 17:38:12 +0800

    扩展

--- library/src/main/java/com/smartown/library/widget/LEDView.java
+++ library/src/main/java/com/smartown/library/widget/LEDView.java
@@ -8,16 +8,18 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.smartown.library.common.tool.LogTool;
+import com.smartown.library.common.tool.Tool;
 import com.smartown.note.library.R;
 
 /**
  * 作者：Tiger
- * <p/>
+ * <p>
  * 时间：2016-09-14 10:50
- * <p/>
+ * <p>
  * 描述：
  */
-public class LEDView extends View {
+public abstract class LEDView extends View {
 
     private int color;
     private String number;
@@ -26,6 +28,10 @@ public class LEDView extends View {
     private int pointWidth;
     //间隔大小
     private int intervalWidth;
+    //每行最多现实多少个字符
+    private int maxColumnSize;
+    //每行的高度
+    private int rowHeight;
 
     public LEDView(Context context) {
         this(context, null);
@@ -56,9 +62,40 @@ public class LEDView extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        LogTool.i("onMeasure", "onMeasure");
+        rowHeight = (getRowNum() + 2) * (pointWidth + intervalWidth);
+        int rowWidth;
+        int rowSize = 1;
+        maxColumnSize = (Tool.getScreenWidth(getContext()) / (pointWidth + intervalWidth) - 1) / (getColumnNum() + 1);
         int length = number.length();
-        setMeasuredDimension(((length * 5) + 1) * pointWidth + (length * 3) * intervalWidth, 9 * pointWidth + 6 * intervalWidth);
+        if (length > maxColumnSize) {
+            rowWidth = Tool.getScreenWidth(getContext());
+            if (length % maxColumnSize > 0) {
+                rowSize = length / maxColumnSize + 1;
+            } else {
+                rowSize = length / maxColumnSize;
+            }
+        } else {
+            rowWidth = ((length * (getColumnNum() + 1)) + 1) * (pointWidth + intervalWidth);
+        }
+        setMeasuredDimension(rowWidth, rowSize * rowHeight);
     }
+
+    /**
+     * @return 行数
+     */
+    protected abstract int getRowNum();
+
+    /**
+     * @return 列数
+     */
+    protected abstract int getColumnNum();
+
+    /**
+     * @param s
+     * @return 绘图点阵数组
+     */
+    protected abstract int[][] getPointsArray(String s);
 
     //    @Override
 //    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -84,444 +121,35 @@ public class LEDView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int startX = pointWidth;
+        LogTool.i("onMeasure", "onDraw");
+        int x = pointWidth + intervalWidth;
+        int y = pointWidth + intervalWidth;
         final int length = number.length();
         for (int i = 0; i < length; i++) {
-            drawNumber(number.substring(i, i + 1), startX, canvas);
-            startX += (5 * pointWidth + 3 * intervalWidth);
+            drawPoints(getPointsArray(number.substring(i, i + 1)), x, y, canvas);
+            if ((i + 1) % maxColumnSize == 0) {
+                x = pointWidth + intervalWidth;
+                y = pointWidth + intervalWidth + ((i + 1) / maxColumnSize * rowHeight);
+            } else {
+                x += ((getColumnNum() + 1) * (pointWidth + intervalWidth));
+            }
         }
     }
 
-    private void drawNumber(String number, int startX, Canvas canvas) {
-        int[][] points;
-        switch (number) {
-            case "0":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "1":
-                points = new int[][]{
-                        {0, 0, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 1, 1, 1}};
-                break;
-            case "2":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {0, 0, 0, 1},
-                        {0, 0, 1, 0},
-                        {0, 1, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 1}};
-                break;
-            case "3":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {0, 0, 0, 1},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "4":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 1},
-                        {0, 0, 0, 1},
-                        {0, 0, 0, 1},
-                        {0, 0, 0, 1}};
-                break;
-            case "5":
-                points = new int[][]{
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 0},
-                        {0, 0, 0, 1},
-                        {0, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "6":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "7":
-                points = new int[][]{
-                        {1, 1, 1, 1},
-                        {0, 0, 0, 1},
-                        {0, 0, 0, 1},
-                        {0, 0, 1, 0},
-                        {0, 1, 0, 0},
-                        {0, 1, 0, 0},
-                        {0, 1, 0, 0}};
-                break;
-            case "8":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "9":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 1},
-                        {0, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case ".":
-                points = new int[][]{
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 1, 0}};
-                break;
-            case ":":
-                points = new int[][]{
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 0},
-                        {0, 0, 0, 0}};
-                break;
-            case "a":
-            case "A":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1}};
-                break;
-            case "b":
-            case "B":
-                points = new int[][]{
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 0}};
-                break;
-            case "c":
-            case "C":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "d":
-            case "D":
-                points = new int[][]{
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 0}};
-                break;
-            case "e":
-            case "E":
-                points = new int[][]{
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 1}};
-                break;
-            case "f":
-            case "F":
-                points = new int[][]{
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0}};
-                break;
-            case "g":
-            case "G":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 0},
-                        {1, 0, 1, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "h":
-            case "H":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1}};
-                break;
-            case "i":
-            case "I":
-                points = new int[][]{
-                        {0, 1, 1, 1},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 1, 1, 1}};
-                break;
-            case "j":
-            case "J":
-                points = new int[][]{
-                        {1, 1, 1, 1},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 1, 0},
-                        {1, 0, 1, 0},
-                        {0, 1, 0, 0}};
-                break;
-            case "k":
-            case "K":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 1, 0},
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 0},
-                        {1, 1, 0, 0},
-                        {1, 0, 1, 0},
-                        {1, 0, 0, 1}};
-                break;
-            case "l":
-            case "L":
-                points = new int[][]{
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 1, 1, 1}};
-                break;
-            case "m":
-            case "M":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1}};
-                break;
-            case "n":
-            case "N":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 1, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1}};
-                break;
-            case "o":
-            case "O":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "p":
-            case "P":
-                points = new int[][]{
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0},
-                        {1, 0, 0, 0}};
-                break;
-            case "q":
-            case "Q":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 0, 1, 1},
-                        {0, 1, 1, 1}};
-                break;
-            case "r":
-            case "R":
-                points = new int[][]{
-                        {1, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 0},
-                        {1, 0, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1}};
-                break;
-            case "s":
-            case "S":
-                points = new int[][]{
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 0},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "t":
-            case "T":
-                points = new int[][]{
-                        {1, 1, 1, 1},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0}};
-                break;
-            case "u":
-            case "U":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0}};
-                break;
-            case "v":
-            case "V":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0}};
-                break;
-            case "w":
-            case "W":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {1, 1, 1, 1},
-                        {1, 0, 0, 1}};
-                break;
-            case "x":
-            case "X":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0},
-                        {0, 0, 0, 0},
-                        {0, 1, 1, 0},
-                        {1, 0, 0, 1},
-                        {1, 0, 0, 1}};
-                break;
-            case "y":
-            case "Y":
-                points = new int[][]{
-                        {1, 0, 0, 1},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0},
-                        {0, 1, 1, 0}};
-                break;
-            case "z":
-            case "Z":
-                points = new int[][]{
-                        {1, 1, 1, 1},
-                        {0, 0, 0, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 0},
-                        {0, 2, 0, 0},
-                        {0, 0, 0, 0},
-                        {1, 0, 0, 1}};
-                break;
-            default:
-                points = new int[7][4];
-                break;
-        }
-        drawPoints(points, startX, canvas);
-    }
-
-    private void drawPoints(int[][] points, int startX, Canvas canvas) {
-        for (int i = 0; i < 7; i++) {
+    private void drawPoints(int[][] points, int x, int y, Canvas canvas) {
+        for (int i = 0; i < getRowNum(); i++) {
             final int[] row = points[i];
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < getColumnNum(); j++) {
                 if (row[j] == 1) {
-                    drawPoint(i, j, startX, canvas);
+                    drawPoint(i, j, x, y, canvas);
                 }
             }
         }
     }
 
-    private void drawPoint(int column, int row, int startX, Canvas canvas) {
-        final int left = startX + (row * (pointWidth + intervalWidth));
-        final int top = column * (pointWidth + intervalWidth) + pointWidth;
+    private void drawPoint(int column, int row, int x, int y, Canvas canvas) {
+        final int left = x + (row * (pointWidth + intervalWidth));
+        final int top = y + (column * (pointWidth + intervalWidth));
         canvas.drawRect(left, top, left + pointWidth, top + pointWidth, paint);
     }
 
