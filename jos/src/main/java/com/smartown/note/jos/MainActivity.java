f8289ad0f85af451dc9087c1407ce406af935181
Tiger <1076192306@qq.com>
2016-10-09 16:32:13 +0800

    RxJava Test

--- jos/src/main/java/com/smartown/note/jos/MainActivity.java
+++ jos/src/main/java/com/smartown/note/jos/MainActivity.java
@@ -1,22 +1,65 @@
 package com.smartown.note.jos;
 
+import android.widget.TextView;
+
 import com.smartown.library.base.BaseActivity;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
 
+    private TextView textView;
+
     @Override
     protected void init() {
-
+        findViews(R.layout.activity_main);
     }
 
     @Override
     protected void findViews() {
-
+        textView = (TextView) findViewById(R.id.text);
     }
 
     @Override
     protected void initViews() {
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
 
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                textView.setText(s);
+            }
+        };
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        subscriber.onNext(String.valueOf(i));
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        subscriber.onError(e);
+                    }
+                }
+                subscriber.onCompleted();
+            }
+        });
+        observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
     }
 
     @Override
