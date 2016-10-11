611f72c8af7b6989601986dd94689d62acc277bf
Tiger <1076192306@qq.com>
2016-10-11 15:15:19 +0800

    RxJava Test

--- jos/src/main/java/com/smartown/note/jos/MainActivity.java
+++ jos/src/main/java/com/smartown/note/jos/MainActivity.java
@@ -4,9 +4,15 @@ import android.widget.TextView;
 
 import com.smartown.library.base.BaseActivity;
 
+import java.util.ArrayList;
+
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
@@ -25,6 +31,110 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initViews() {
+        flatMap();
+    }
+
+    @Override
+    protected void registerViews() {
+
+    }
+
+    /**
+     * 创建观察者
+     */
+    private void observer() {
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+        };
+    }
+
+    /**
+     * 创建观察者
+     */
+    private void subscriber() {
+        Subscriber<String> subscriber = new Subscriber<String>() {
+
+            @Override
+            public void onStart() {
+                super.onStart();
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+        };
+        subscriber.unsubscribe();
+    }
+
+    /**
+     * 创建被观察者
+     */
+    private void create() {
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+//                subscriber.onNext("Hello");
+//                subscriber.onNext("Hi");
+//                subscriber.onNext("Aloha");
+//                subscriber.onCompleted();
+            }
+        });
+    }
+
+
+    /**
+     * create() 方法是 RxJava 最基本的创造事件序列的方法。基于这个方法， RxJava 还提供了一些方法用来快捷创建事件队列
+     */
+    private void just() {
+        Observable observable = Observable.just("Hello", "Hi", "Aloha");
+// 将会依次调用：
+// onNext("Hello");
+// onNext("Hi");
+// onNext("Aloha");
+// onCompleted();
+    }
+
+    /**
+     * just(T...) 的例子和 from(T[]) 的例子，都和之前的 create(OnSubscribe) 的例子是等价的。
+     */
+    private void from() {
+        String[] words = {"Hello", "Hi", "Aloha"};
+        Observable observable = Observable.from(words);
+// 将会依次调用：
+// onNext("Hello");
+// onNext("Hi");
+// onNext("Aloha");
+// onCompleted();
+    }
+
+    /**
+     * 订阅
+     */
+    private void subscribe() {
         Subscriber<String> subscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
@@ -38,21 +148,14 @@ public class MainActivity extends BaseActivity {
 
             @Override
             public void onNext(String s) {
-                textView.setText(s);
+                System.out.println(s);
             }
         };
         Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
-                for (int i = 0; i < 10; i++) {
-                    try {
-                        subscriber.onNext(String.valueOf(i));
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        subscriber.onError(e);
-                    }
-                }
+                subscriber.onNext("HELLO");
+                subscriber.onNext("WORLD");
                 subscriber.onCompleted();
             }
         });
@@ -62,9 +165,102 @@ public class MainActivity extends BaseActivity {
                 .subscribe(subscriber);
     }
 
-    @Override
-    protected void registerViews() {
+    /**
+     * 订阅
+     * 除了 subscribe(Observer) 和 subscribe(Subscriber) ，subscribe() 还支持不完整定义的回调，RxJava 会自动根据定义创建出 Subscriber 。
+     */
+    private void action() {
+        //onNext
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println(s);
+            }
+        };
+        //onError
+        Action1<Throwable> onErrorAction = new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
 
+            }
+        };
+        //onComplete
+        Action0 onCompleteAction = new Action0() {
+            @Override
+            public void call() {
+
+            }
+        };
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("HELLO");
+                subscriber.onNext("WORLD");
+                subscriber.onCompleted();
+            }
+        });
+        observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onNextAction, onErrorAction, onCompleteAction);
+    }
+
+    private void map() {
+        Integer[] integers = {1, 2, 3, 4, 5};
+        Observable.from(integers)
+                .map(new Func1<Integer, String>() {
+                    @Override
+                    public String call(Integer integer) {
+                        return String.valueOf(integer.intValue());
+                    }
+                })
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.println(s);
+                    }
+                });
+    }
+
+    private void flatMap() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        ArrayList[] lists = {list, list, list};
+        Observable.from(lists)
+                .flatMap(new Func1<ArrayList, Observable<Integer>>() {
+                    @Override
+                    public Observable<Integer> call(ArrayList arrayList) {
+                        return Observable.from(arrayList);
+                    }
+                })
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println(integer.intValue() + "");
+                    }
+                });
     }
 
 }
